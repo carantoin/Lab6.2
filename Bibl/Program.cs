14325c9f9c645816dd090eb96d5cab9077be90d7
carantoin <90127329+carantoin@users.noreply.github.com>
2024-12-26 18:26:26 +0200

    Add files via upload

---
+++ Bibl/Program.cs
@@ -0,0 +1,13 @@
+﻿using static ClassLibrary1.ClassLib;
+Random random = new Random();
+int[,] mas = new int[7, 6];
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        mas[i, j] = random.Next(10, 100);
+        Console.WriteLine(mas[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Среднее арифметическое элемнтов:" + IndexAverage(mas));
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int IndexAverage(int[,] mas)
+        {
+            int average = mas[0, 0];
+            int index = -1;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    if (mas[i, j] > average)
+                    {
+                        average = mas[i, j];
+                        index = i + j / mas.GetLength(0);
+                    }
+                }
+            }
+            return index;
+        }
+    }
+}
